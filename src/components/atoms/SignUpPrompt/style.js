2451dbb6709d1fe80b87f4d600b270c5c4432290
RSWeslei <weslei67rosiak.com@gmail.com>
2023-08-13 16:51:10 -0300

    Signin continued

---
+++ src/components/atoms/SignUpPrompt/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import colors from "../../utils/globalColors";
+import SignUpPrompt from "./index";
+
+const SignUpPromptStyle = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: "column",
+  },
+})
+
+export default SignUpPromptStyle;
